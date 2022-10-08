1dbc930078296bf3da27b5d494cf08949b2648e4
mayukan <leemy3023@gmail.com>
2022-10-08 01:03:12 +0000

    create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'homes/top'
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:new, :index, :show, :create, :destroy
-  ]
+  resources :books, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
