2f1d6bfbdfb5469a0001316296811f99173fff24
mayukan <leemy3023@gmail.com>
2022-10-25 10:53:43 +0000

    IndexPage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to @book
   end
 
   def index
@@ -17,7 +17,7 @@ class BooksController < ApplicationController
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def destroy
