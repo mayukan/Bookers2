0b2c82d29b8da681137bf4880b62b5c826361bb4
mayukan <leemy3023@gmail.com>
2022-10-26 10:47:01 +0000

    routes-resources-postimages

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :books, only: [:new, :index, :show, :create, :destroy]
   resources :users, only: [:show, :edit]
+  resources :post_images, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
