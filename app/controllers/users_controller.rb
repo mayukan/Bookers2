276e1f10ffe0c6bdfbbf2f451791b9299152bb4f
mayukan <leemy3023@gmail.com>
2022-10-28 11:32:05 +0000

    UserEditPage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @user = User.find(params[:id])
+
   end
 end
