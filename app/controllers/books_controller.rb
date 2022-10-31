ca0a0e103251d4bba29315b25ebdb73d1889ca6d
mayukan <leemy3023@gmail.com>
2022-10-31 11:22:40 +0000

    flashmessages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+   if @book.save
+    redirect_to book_path(@book), notice: "You have created book successfully."
+   end
   end
 
   def index
