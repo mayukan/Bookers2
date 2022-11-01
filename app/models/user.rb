8eca36c692beb5009c6a9fafc2f4a10f40847dfb
mayukan <leemy3023@gmail.com>
2022-11-01 13:22:00 +0000

    Routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
 
-  has_one_attached :image
+  has_one_attached :profile_image
 
   validates :name, length: { in: 2..20 }, uniqueness: true
-  validates :introduction, length: {maximum: 50}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
 
 def get_profile_image(width, height)
-  unless image.attached?
+  unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpeg')
-    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
-    image.variant(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'homes/top'
   get "homes/about" => "homes#about"
   devise_for :users
   root to: "homes#top"
