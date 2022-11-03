1c272ba9abbebba42e2504cb7830e2e8c2b2bae0
mayukan <leemy3023@gmail.com>
2022-11-03 09:55:44 +0000

    UserIndexPage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+  def index
+    @book = Book.new
+    @user = current_user
+    @users = User.all
+  end
+
   def show
     @book = Book.new
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
-  resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
